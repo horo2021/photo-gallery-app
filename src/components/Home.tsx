04c4293ff2b7cb046c1a97b1ba3e554be4970b65
hamza <hamzah.i.hamad@gmail.com>
2022-08-25 15:31:41 +0300

    adding a home component which will render the content of the project

---
+++ src/components/Home.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const Home = () => {
+  return <Container></Container>;
+};
+
+export default Home;
+
+const Container = styled.main`
+  padding: 0px 30px;
+
+  background-image: linear-gradient(
+    to right top,
+    #051937,
+    #0e1831,
+    #12172c,
+    #151626,
+    #161521
+  );
+`;
